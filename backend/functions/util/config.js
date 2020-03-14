5cbc1d6c893d4080085810bf86a2d027217fd5d6
Mark Zhu <jinyue98@uw.edu>
2020-03-14 12:08:15 -0700

    adding music functionality

--- backend/functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	apiKey: "AIzaSyA-XSMg80jHERCcYvh7YW1YD95nzuQ_O_M",
-	authDomain: "bluebook-5f792.firebaseapp.com",
-	databaseURL: "https://bluebook-5f792.firebaseio.com",
-	projectId: "bluebook-5f792",
-	storageBucket: "bluebook-5f792.appspot.com",
-	messagingSenderId: "1055419950403",
-	appId: "1:1055419950403:web:c8dc875b43d986ac6abbf6"
-};
